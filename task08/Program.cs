b11a29c7e980472ffd44e75eea56fbff051eef2f
KonstantinVasiliev <grafit21@mail.ru>
2022-10-17 16:21:39 +0500

    Выполнена задача Task08

---
+++ task08/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число N: ");
+String Firstnum = Console.ReadLine();  // Ввод  числа
+int n = Convert.ToInt32(Firstnum); // Преобразование строки c числом N для числа nd
+Console.WriteLine("Четные числа в заданном цикле: ");
+for (int a = 1; a < n; a++ )
+{
+    if (a % 2 == 0)
+    {
+            Console.WriteLine(a);
+    }
+}
