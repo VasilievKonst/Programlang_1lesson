9835c95cf4c6ce1337b42afbabdfe0eedfb8b9f6
KonstantinVasiliev <grafit21@mail.ru>
2022-10-17 15:22:24 +0500

    Выполнена задача Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число: ");
+String Firstnum = Console.ReadLine();  // Ввод первого числа
+int Numb1 = Convert.ToInt32(Firstnum); // Преобразование строки в число для первого числа
+Console.WriteLine("Введите второе число: ");
+String Secondnum = Console.ReadLine();  // Ввод второго числа
+int Numb2 = Convert.ToInt32(Secondnum); // Преобразование строки в число для второго числа
+Console.WriteLine("Введите третье число: ");
+String Thirdnum = Console.ReadLine();  // Ввод третьего числа
+int Numb3 = Convert.ToInt32(Thirdnum); // Преобразование строки в число для третьего числа
+int Max = Numb1;
+if (Numb1 > Max) Max = Numb1;
+if (Numb2 > Max) Max = Numb2;
+if (Numb3 > Max) Max = Numb3;
+Console.Write("Max = ");
+Console.WriteLine(Max);
