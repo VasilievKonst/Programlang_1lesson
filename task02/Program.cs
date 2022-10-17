02d23744bf362114493a51b929dbbda4941b9207
KonstantinVasiliev <grafit21@mail.ru>
2022-10-17 15:09:41 +0500

    Выполнена задача Task02

---
+++ task02/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число: ");
+String Firstnum = Console.ReadLine();  // Ввод первого числа
+int Numb1 = Convert.ToInt32(Firstnum); // Преобразование строки в число для первого числа
+Console.WriteLine("Введите второе число: ");
+String Secondnum = Console.ReadLine();  // Ввод второго числа
+int Numb2 = Convert.ToInt32(Secondnum); // Преобразование строки в число для второго числа
+if (Numb1 > Numb2)
+{
+    Console.Write("Max = ");
+    Console.WriteLine(Numb1);
+}
+if (Numb2 > Numb1)
+{
+    Console.Write("Max = ");
+    Console.WriteLine(Numb2);
+}
+else
+{
+    Console.WriteLine("Числа равны!");
+}
